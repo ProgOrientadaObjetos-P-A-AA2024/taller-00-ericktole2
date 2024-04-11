d1375ca334aedcdd8f72438a598f03954fea25cd
erick <estoledo1@utpl.edu.ec>
2024-04-11 17:06:09 -0500

    taller0.2

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -7,6 +7,8 @@ package paquete2;
 
 import paquete1.Mensaje;
 import paquete3.Informacion;
+import java.util.Locale;
+import java.util.Scanner;
 
 /**
  *
@@ -18,11 +20,26 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         // Despues de impotar las clases, utilizamos un prinf y llamamos las 
-         // funciones para que de esa manera
-         // poder completar la frase deseada
-        System.out.printf("%s con %d\n", Mensaje.obtenerMensaje(),
-                 Informacion.obtenerHabitantes());
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String cadena = " ";
+        int num;
+        // Despues de impotar las clases, utilizamos un prinf y llamamos las 
+        // funciones para que de esa manera
+        // poder completar la frase deseada
+        cadena = String.format("%s con %d\n", Mensaje.obtenerMensaje(),
+                Informacion.obtenerHabitantes());
+        System.err.println("ingrese un numero ");
+        num = entrada.nextInt();
+        if (num <= 5) {
+            System.out.println(cadena);
+
+        } else {
+        
+        System.out.printf("%s",cadena.toUpperCase());
+
     }
+}
 
 }
---
+++ Demo/src/paquete2/Demo002.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+
+import paquete1.Mensaje;
+import paquete3.Informacion;
+
+/**
+ *
+ * @author reroes
+ */
+public class Demo002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // Despues de impotar las clases, utilizamos un prinf y llamamos las 
+         // funciones para que de esa manera
+         // poder completar la frase deseada
+         
+        System.out.printf("%s con %d\n", Mensaje.obtenerMensaje(),
+                 Informacion.obtenerHabitantes());
+    }
+
+}
