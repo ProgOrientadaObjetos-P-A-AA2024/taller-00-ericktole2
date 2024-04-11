52512137eb65d495fed6b8f92fe22d164f1efc63
erick <estoledo1@utpl.edu.ec>
2024-04-11 16:46:05 -0500

    taller0.1

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package paquete2;
+
 import paquete1.Mensaje;
 import paquete3.Informacion;
+
 /**
  *
  * @author reroes
@@ -16,9 +18,11 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        System.out.printf("%s con %d\n",Mensaje.obtenerMensaje()
-                ,Informacion.obtenerHabitantes());
+         // Despues de impotar las clases, utilizamos un prinf y llamamos las 
+         // funciones para que de esa manera
+         // poder completar la frase deseada
+        System.out.printf("%s con %d\n", Mensaje.obtenerMensaje(),
+                 Informacion.obtenerHabitantes());
     }
-    
+
 }
